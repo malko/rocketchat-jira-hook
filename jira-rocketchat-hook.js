9fff064d37c77aff17e22a66c4c86b169e85951b
Soroush Kaviani <90821955+srkaviani@users.noreply.github.com>
2024-09-01 17:00:58 +0330

    Add Comment Create and Edit Notifs

--- jira-rocketchat-hook.js
+++ jira-rocketchat-hook.js
@@ -57,7 +57,25 @@ class Script {
 			} else if (data.webhookEvent === 'jira:issue_deleted') {
 				message.attachments.push(prepareAttachment(data, `*Deleted* ${issueSummary}`));
 			} else if (data.webhookEvent === 'jira:issue_updated') {
-				if (data.changelog && data.changelog.items) { // field update
+				if (data.issue_event_type_name === 'issue_commented') {
+				let comment = data.comment;
+				let commentAuthoring = comment.author && comment.author.displayName ? ` by ${comment.author.displayName}` : '';
+				data.user = comment.author;
+				message.attachments.push(prepareAttachment(data, `Comment created for ${issueSummary}:\n\`\`\`\n${stripDesc(comment.body)}\n\`\`\`\n${commentAuthoring}`));
+				}
+				else if (data.issue_event_type_name === 'issue_comment_deleted') {
+					let comment = data.comment;
+					let commentAuthoring = comment.author && comment.author.displayName ? ` by ${comment.author.displayName}` : '';
+					data.user = comment.author;
+					message.attachments.push(prepareAttachment(data, `Comment deleted for ${issueSummary}:\n\`\`\`\n${stripDesc(comment.body)}\n\`\`\`\n${commentAuthoring}`));	
+				}
+				else if (data.issue_event_type_name === 'issue_comment_edited') {
+					let comment = data.comment;
+					let commentAuthoring = comment.author && comment.author.displayName ? ` by ${comment.author.displayName}` : '';
+					data.user = comment.author;
+					message.attachments.push(prepareAttachment(data, `Comment updated for ${issueSummary}:\n\`\`\`\n${stripDesc(comment.body)}\n\`\`\`\n${commentAuthoring}`));
+					}
+				else if (data.changelog && data.changelog.items) { // Comment
 					let logs = [];
 					data.changelog.items.forEach((change) => {
 						if (change.field === 'description') {
@@ -81,7 +99,7 @@ class Script {
 					}
 					return;
 				}
-			} else if (data.webhookEvent === 'comment_created') {
+			} else if (data.webhookEvent === 'issue_commented') {
 				let comment = data.comment;
 				let commentAuthoring = comment.author && comment.author.displayName ? ` by ${comment.author.displayName}` : '';
 				data.user = comment.author;
